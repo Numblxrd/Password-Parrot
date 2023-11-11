4494e5ff48fef2dba64c123ea1555969046a7505
Numblxrd <arsalan202202@gmail.com>
2023-11-11 07:47:24 +0330

    1st commit

---
+++ password_parrot.py
@@ -0,0 +1,46 @@
+import random
+import pyperclip
+
+print(r'''
+   
+ █▀█ ▄▀█ █▀ █▀ █░█░█ █▀█ █▀█ █▀▄   █▀█ ▄▀█ █▀█ █▀█ █▀█ ▀█▀
+ █▀▀ █▀█ ▄█ ▄█ ▀▄▀▄▀ █▄█ █▀▄ █▄▀   █▀▀ █▀█ █▀▄ █▀▄ █▄█ ░█░
+ 
+                         __,---.
+                        /__|o\  )
+                         `-\ / /
+                           ,) (,
+                          //   \\
+                         {(     )}
+                   =======""===""=======
+                           |||||
+                            |||
+                             |
+      
+              [Just A Simple Password Generator]
+                        [By Numblxrd]
+''')
+
+characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ1234567890!@#$%^&*?'
+
+def generate_a_password():
+
+    password = ''.join([random.choice(characters) for _ in range(how_many)])
+
+    return password
+
+while True:
+
+  how_many = int(input('\nHow Many Characters You Want For Password? '))
+
+  generated_password = generate_a_password()
+
+  print('\n--->',generated_password)
+
+  pyperclip.copy(generated_password)
+
+  print('\nThe Password Copied To Your Clipboard!\n')
+
+  print('----------------------------------------')
+
+input()
